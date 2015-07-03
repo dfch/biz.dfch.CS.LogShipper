e471086ca0437f49997f8d4e6301a06b98996859
Ronald Rink <ronald.rink@d-fens.net>
2015-07-03 07:29:01 +0200

    helper for NameValueCollection comparison

---
+++ LogShipper.Tests/Helpers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace biz.dfch.CS.LogShipper.Tests
+{
+    class Helpers
+    {
+        internal static bool CompareNameValueCollections
+            (
+            NameValueCollection left
+            ,
+            NameValueCollection right
+            ,
+            bool exactOrder
+            )
+        {
+            var leftKeys = left.AllKeys;
+            var rightKeys = right.AllKeys;
+            if (leftKeys.Count() != rightKeys.Count())
+            {
+                return false;
+            }
+            if (exactOrder)
+            {
+                return leftKeys
+                        .SequenceEqual(rightKeys)
+                            && leftKeys.All(key => left[key] == right[key]);
+            }
+            else
+            {
+                return leftKeys
+                    .OrderBy(key => key)
+                        .SequenceEqual(rightKeys.OrderBy(key => key))
+                            && leftKeys.All(key => left[key] == right[key]);
+            }
+        }
+    }
+}
